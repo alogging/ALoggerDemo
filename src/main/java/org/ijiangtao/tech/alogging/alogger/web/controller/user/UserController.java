9f2524c7808bb122613fc5360e52b5187a69932a
ijiangtao <jiangtao.li@baozun.com>
2019-01-29 18:44:07 +0800

    swagger jetty demo init

---
+++ src/main/java/org/ijiangtao/tech/alogging/alogger/web/controller/user/UserController.java
@@ -0,0 +1,12 @@
+package org.ijiangtao.tech.alogging.alogger.web.controller.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.Api;
+
+@Api(value = "UserController")
+@RestController
+public class UserController {
+
+	
+}
